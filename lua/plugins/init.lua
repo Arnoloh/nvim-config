0d29a954aec1fc2cb796998afcf15a3cafb070e9
Arnoloh <arnaud.corcione@epita.fr>
2023-01-12 16:13:53 +0100

    update config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,60 +1,48 @@
-local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-local compiled_path = fn.stdpath('config')..'/plugin'
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  fn.system({'rm', '-r', compiled_path})
-  vim.cmd [[packadd packer.nvim]]
-end
-
 vim.cmd([[packadd packer.nvim]])
 
 return require("packer").startup(function(use)
-	use("wbthomason/packer.nvim")
+    use("wbthomason/packer.nvim")
 
-	-- LSP & Completion
-	use("onsails/lspkind-nvim")
-	use("hrsh7th/cmp-buffer")
+    -- LSP & Completion
+    use("onsails/lspkind-nvim")
+    use("hrsh7th/cmp-buffer")
     use("hrsh7th/cmp-path")
-	use("hrsh7th/cmp-nvim-lsp")
-	use("hrsh7th/nvim-cmp")
-	use("neovim/nvim-lspconfig")
-	use("glepnir/lspsaga.nvim")
-	use("L3MON4D3/LuaSnip")
-	use("simrat39/rust-tools.nvim")
-	use("windwp/nvim-autopairs")
+    use("hrsh7th/cmp-nvim-lsp")
+    use("hrsh7th/nvim-cmp")
+    use("neovim/nvim-lspconfig")
+    use("glepnir/lspsaga.nvim")
+    use("L3MON4D3/LuaSnip")
+    use("simrat39/rust-tools.nvim")
+    use("windwp/nvim-autopairs")
 
-	use("lewis6991/gitsigns.nvim")
-	use("glepnir/dashboard-nvim")
-	use({
-		"kyazdani42/nvim-tree.lua",
-		requires = {
-			"kyazdani42/nvim-web-devicons",
-		},
-	})
-	use("sbdchd/neoformat")
-	use({
-		"nvim-telescope/telescope.nvim",
-		requires = {
-			"nvim-lua/plenary.nvim",
-		},
-	})
-	use({
-		"nvim-lualine/lualine.nvim",
-		requires = { "kyazdani42/nvim-web-devicons" },
-	})
-	use("karb94/neoscroll.nvim")
-	use("B4mbus/oxocarbon-lua.nvim")
-	use("navarasu/onedark.nvim")
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		run = ":TSUpdate",
-	})
-	use({ "akinsho/bufferline.nvim", tag = "v2.*", requires = "kyazdani42/nvim-web-devicons" })
-	use("dstein64/vim-startuptime")
+    use("lewis6991/gitsigns.nvim")
+    use("glepnir/dashboard-nvim")
+    use({
+        "kyazdani42/nvim-tree.lua",
+        requires = {
+            "kyazdani42/nvim-web-devicons",
+        },
+    })
+    use("sbdchd/neoformat")
+    use({
+        "nvim-telescope/telescope.nvim",
+        requires = {
+            "nvim-lua/plenary.nvim",
+        },
+    })
+    use({
+        "nvim-lualine/lualine.nvim",
+        requires = { "kyazdani42/nvim-web-devicons" },
+    })
+    use("karb94/neoscroll.nvim")
+    use("B4mbus/oxocarbon-lua.nvim")
+    use("navarasu/onedark.nvim")
+    use({
+        "nvim-treesitter/nvim-treesitter",
+        run = ":TSUpdate",
+    })
+    use({ "akinsho/bufferline.nvim", tag = "v2.*", requires = "kyazdani42/nvim-web-devicons" })
+    use("dstein64/vim-startuptime")
 
-	require("plugins/startup")
-     if packer_bootstrap then
-        require('packer').sync()
-    end
+    require("plugins/startup")
 end)
