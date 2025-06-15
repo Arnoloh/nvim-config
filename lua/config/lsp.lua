5167df6eb26b486e27df255de884da4b41ce3d5e
Arnoloh <arnaud.corcione@epita.fr>
2025-06-15 12:57:32 +0200

    removing decapreated version

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,5 +1,5 @@
 require("lspconfig").clangd.setup({})
-require("lspconfig").tsserver.setup({
+require("lspconfig").ts_ls.setup({
  on_attach = on_attach,
   filetypes = { "typescript", "typescriptreact", "typescript.tsx", "javascript", "javascriptreact", "javascript.jsx" },
   capabilities = capabilities
