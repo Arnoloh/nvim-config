e747e501fec98382b1a2159c9e585be87036dbaf
Arnoloh <arnaud.corcione@epita.fr>
2025-10-01 11:55:21 +0200

    fix(lsp): update for nvim 0.11

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,9 +1,20 @@
-local lspconfig = require('lspconfig')
-lspconfig.clangd.setup({})
-lspconfig.pyright.setup({})
-lspconfig.ts_ls.setup({
- on_attach = on_attach,
-  filetypes = { "typescript", "typescriptreact", "typescript.tsx", "javascript", "javascriptreact", "javascript.jsx" },
-  capabilities = capabilities
+-- (Optionnel mais recommandé) s’assurer que les configs par défaut de nvim-lspconfig
+-- sont sur le runtimepath, sans utiliser require('lspconfig').
+pcall(require, "nvim-lspconfig")
+
+-- Déclarations/overrides inline
+vim.lsp.config("clangd", {})
+
+vim.lsp.config("pyright", {})
+
+vim.lsp.config("ts_ls", {
+  on_attach = on_attach,
+  filetypes = {
+    "typescript", "typescriptreact", "typescript.tsx",
+    "javascript", "javascriptreact", "javascript.jsx",
+  },
+  capabilities = capabilities,
 })
 
+-- Activer les serveurs
+vim.lsp.enable({ "clangd", "pyright", "ts_ls" })
